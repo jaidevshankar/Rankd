3933d27f5ecbccef8ada923c5b14c1a53f5bbc77
leon-chien <chienleonllc@gmail.com>
2025-03-14 09:02:00 -0500

    Created fetch album for spotidy api

--- backend/spotify_api.py
+++ backend/spotify_api.py
@@ -1,30 +1,23 @@
 import os
 from dotenv import load_dotenv
 import spotipy
-from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
 
 load_dotenv()
 
-class SpotifyClient:
-    def __init__(self):
-        self.client = spotipy.Spotify(auth_manager=SpotifyOAuth(
-            client_id=os.getenv('SPOTIFY_CLIENT_ID'),
-            client_secret=os.getenv('SPOTIFY_CLIENT_SECRET'),
-            redirect_uri=os.getenv('SPOTIFY_REDIRECT_URI'),
-            scope='user-library-read playlist-modify-public user-top-read'
-        ))
-    
-    def get_user_top_tracks(self, limit=20, offset=0, time_range='medium_term'):
-        """
-        Get user's top tracks
-        time_range: short_term (4 weeks), medium_term (6 months), long_term (all time)
-        """
-        return self.client.current_user_top_tracks(
-            limit=limit,
-            offset=offset,
-            time_range=time_range
-        )
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id="30b50adac3584635a327735395823eb7",
+    client_secret="7471275b93e44c929743e06936760cc5"
+))
 
-    def get_user_playlists(self, limit=50):
-        """Get user's playlists"""
-        return self.client.current_user_playlists(limit=limit)
+def fetch_album(album_id: str):
+    album_data = sp.album(album_id)
+    return {
+        "item_id": album_data["id"],
+        "item_name": album_data["name"],
+        "metadata": {
+            "artist": album_data["artists"][0]["name"],
+            "release_date": album_data["release_date"],
+            "tracks": [track["name"] for track in album_data["tracks"]["items"]]
+        }
+    }

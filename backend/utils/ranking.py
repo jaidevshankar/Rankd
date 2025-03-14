59995fcd8afe3e0a0ea8961bd9d201793743d263
leon-chien <chienleonllc@gmail.com>
2025-03-14 09:08:27 -0500

    Added spotify fetch album

---
+++ backend/utils/ranking.py
@@ -0,0 +1,20 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+# Initialize the Spotify client with credentials
+sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
+    client_id="30b50adac3584635a327735395823eb7",
+    client_secret="7471275b93e44c929743e06936760cc5"
+))
+
+def fetch_album(album_id: str):
+    album_data = sp.album(album_id)
+    return {
+        "item_id": album_data["id"],
+        "item_name": album_data["name"],
+        "metadata": {
+            "artist": album_data["artists"][0]["name"],
+            "release_date": album_data["release_date"],
+            "tracks": [track["name"] for track in album_data["tracks"]["items"]]
+        }
+    }
